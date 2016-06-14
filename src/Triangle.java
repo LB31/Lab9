54c346e2bc5ae45e3ece88b4748195787f70797d
Eli Gould <MariaKatharinaWunder@gmail.com>
2016-06-14 10:49:06 +0200

    Create basic frme

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,13 +1,31 @@
+import java.awt.Color;
+import java.awt.Frame;
 import java.awt.Graphics;
 
-import javax.swing.JFrame;
 
-public class Triangle {
+public class Triangle extends Frame {
+    
+    public Triangle() {
+        setGui();
+    }
 
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("FrameDemo");	
-		frame.pack();
-		frame.setVisible(true);
-	}
+    public void paint(Graphics g){
+        g.setColor(Color.red);
+        g.drawLine(50, 50, 100, 100);
+        System.out.println("Test!");
+    }
+    
+
+    private void setGui() {
+        setTitle("Triangle");
+        setBounds(450, 100, 500, 500);
+        setBackground(Color.WHITE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Triangle myTriangle = new Triangle();
+    }
 
 }
+ 

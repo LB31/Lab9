3d7852edbfabc945140c9876f6e0ef4a35f6abb2
LB31 <leonidb93@gmail.com>
2016-06-14 10:15:47 +0200

    Start

---
+++ src/Triangle.java
@@ -0,0 +1,13 @@
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("FrameDemo");	
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+}

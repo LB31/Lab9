de12b8bce7721e2df7aeeb3fffd26252b31f5090
Eli Gould <MariaKatharinaWunder@gmail.com>
2016-06-14 11:24:10 +0200

    Make full screen

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,21 +1,29 @@
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
 
 public class Triangle extends JPanel {
 
 	public void paint(Graphics g) {
-		g.drawLine(50, 100, 0, 50);
-		// g.setColor(Color.red);
-		// g.drawLine(50, 50, 10, 94);
+		
+	      int[] xcoords = {150, 270, 30};
+	      int[] ycoords = {180, 350, 350};
+	      g.setColor (Color.GREEN);
+	      g.fillPolygon (xcoords, ycoords, xcoords.length);
 
 	}
+	
+	public Triangle(){
+		
+	}
+	
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		frame.getContentPane().add(new Triangle());
-		frame.setSize(300, 200);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setSize(screenSize);
 		frame.setVisible(true);
 	}
 

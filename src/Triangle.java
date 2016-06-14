018ec487378d34e0c4783c4a9aa72f248126ae5b
LB31 <leonidb93@gmail.com>
2016-06-14 10:48:05 +0200

    fancier stuff

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,12 +1,20 @@
 import java.awt.Graphics;
 
-import javax.swing.JFrame;
 
-public class Triangle {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Triangle extends JPanel {
+	
+	
+	public void paint(Graphics g) {
+		g.drawLine(50, 80, 10, 94);
+	}
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("FrameDemo");	
-		frame.pack();
+		JFrame frame = new JFrame();
+		frame.getContentPane().add(new Triangle());
+		frame.setSize(300, 200);
 		frame.setVisible(true);
 	}
 

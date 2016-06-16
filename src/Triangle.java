aa4b6e1faf017d9b9fec5286ad06fe42efb1db7a
LB31 <leonidb93@gmail.com>
2016-06-17 01:47:20 +0200

    clean main, start different colours and recursion

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,22 +1,59 @@
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Triangle extends JPanel {
+	private Dimension screenSize;
+	int height;
+	int width;
+	int layer = 1;
+	Color colours = new Color(255,255,255);
 
+	public Dimension getScreenSize() {
+		return screenSize;
+	}
+	
+	@Override
 	public void paint(Graphics g) {
-		g.drawLine(50, 100, 0, 50);
-		// g.setColor(Color.red);
-		// g.drawLine(50, 50, 10, 94);
+
+		for (int i = 1; i <= layer; i++) {
+			// left, top, right 
+			int[] xcoords = { 0, width / 2, width };
+			int[] ycoords = { height, 0, height };
+			g.setColor(colours);
+			g.fillPolygon(xcoords, ycoords, xcoords.length);
+		}
+		layer *= 3;
+		System.out.println(layer);
+		if (layer <= 27)
+			paint(g);
+
+	}
+
+	public Triangle() {
+		screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		height = (int) screenSize.getHeight();
+		width = (int) screenSize.getWidth();
+	}
+
+	public void createTriangle(Triangle tri) {
+		JFrame frame = new JFrame();
+		frame.getContentPane().add(tri);
+		Dimension sizes = tri.getScreenSize();
+		frame.setSize(sizes);
+		frame.setBackground(Color.RED);
+		frame.setVisible(true);
+
+	}
+
+	public void drawTheTriangles() {
 
 	}
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-		frame.getContentPane().add(new Triangle());
-		frame.setSize(300, 200);
-		frame.setVisible(true);
+		Triangle butz = new Triangle();
+		butz.createTriangle(butz);
+
 	}
 
 }

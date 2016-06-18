b0a938abd2293da1b150bc3a4f9adc451f59c382
Eli Gould <MariaKatharinaWunder@gmail.com>
2016-06-18 11:44:57 +0200

    Make triangle responsive to frame size and write first idea for recursive algorrithm

--- src/Triangle.java
+++ src/Triangle.java
@@ -2,22 +2,44 @@ import java.awt.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-
 public class Triangle extends JPanel {
 
+	private Dimension screenSize;
+	private int screenHeight;
+	private int screenWidth;
+
 	public void paint(Graphics g) {
-		
-	      int[] xcoords = {150, 270, 30};
-	      int[] ycoords = {180, 350, 350};
-	      g.setColor (Color.GREEN);
-	      g.fillPolygon (xcoords, ycoords, xcoords.length);
+
+		screenSize = this.getSize();
+		screenHeight = (int) screenSize.getHeight();
+		screenWidth = (int) screenSize.getWidth();
+
+		int[] xcoords = { 0, screenWidth, (screenWidth) / 2 };
+		int[] ycoords = { screenHeight, screenHeight, 0 };
+		g.setColor(Color.GREEN);
+		g.fillPolygon(xcoords, ycoords, xcoords.length);
+
+		drawRecursiveTriangle(screenWidth, screenHeight, g);
 
 	}
-	
-	public Triangle(){
-		
+
+	public Triangle() {
+		screenSize = null;
+		screenHeight = 0;
+		screenWidth = 0;
 	}
-	
+
+	private void drawRecursiveTriangle(int screenWidth, int screenHeight, Graphics g) {
+
+		if (screenHeight != 0 && screenWidth != 0) {
+			int[] xcoords2 = { screenWidth / 4, screenWidth - (screenWidth / 4), (screenWidth) / 2 };
+			int[] ycoords2 = { screenHeight / 2, screenHeight / 2, screenHeight };
+			g.setColor(Color.BLUE);
+			g.fillPolygon(xcoords2, ycoords2, xcoords2.length);
+
+			drawRecursiveTriangle(screenWidth / 2, screenHeight / 2, g);
+		}
+	}
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
